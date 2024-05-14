3925f4f190de8ce3dc5d097e001ae74c2df1cc32
Anton718 <anton.kostroma@yahoo.com>
2024-05-13 20:45:17 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const port = process.env.port || 8888;
+const fs = require('fs');
+const https = require('https');
+
+const certFile = fs.readFileSync('/etc/letsencrypt/live/express718.ru/cert.pem');
+const keyFile = fs.readFileSync('/etc/letsencrypt/live/express718.ru/privkey.pem');
+
+
+global.bodyParser = require("body-parser");
+global.urlencodedParser = bodyParser.urlencoded({ extended: false });
+global.pgp = require("pg-promise")();
+global.db = pgp("postgres://postgres:postgres@localhost:5432/db1");
+global.path = require("path");
+global.bcrypt = require("bcrypt");
+global.jwt = require("jsonwebtoken");
+global.fileUpload = require("express-fileupload");
+
+const indexGetRoutes = require("./src/index/index.routes.get");
+const blogRoutes = require("./src/blog/blog.routes");
+const authRoutes = require("./src/auth/auth.routes");
+const contactRoute = require("./src/contact/contact.routes");
+const messagesRoutes = require("./src/messages/private.routes");
+const commentsRoutes = require("./src/comments/comments.routes");
+const fileUpload = require("express-fileupload");
+
+app.set("view engine", "ejs");
+
+app.use(cookieParser("rwervterbj353jhbdkfhv"));
+app.use(indexGetRoutes);
+app.use(messagesRoutes);
+app.use(blogRoutes);
+app.use(authRoutes);
+app.use(contactRoute);
+app.use(commentsRoutes);
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.json());
+app.use(fileUpload());
+app.use(express.urlencoded({ extended: false }));
+app.use((req, res) => {
+  res.status(404).send("404 Page does not exist");
+});
+
+let options = {
+   cert: certFile,
+   key: keyFile
+};
+
+https.createServer(options, app).listen(port)
+
+
---
+++ src/index/controllers/profileImage.js
@@ -0,0 +1,18 @@
+exports.addImage = async (req, res) => {
+  const token = req.signedCookies.token;
+  if (token) {
+    const user = jwt.verify(token, "rwervterbj353jhbdkfhv");
+    if (req.files) {
+      let fileName = `${Math.random() * 10000000000000000}${path.extname(
+        req.files.image.name
+      )}`;
+      req.files.image.mv("public/images/uploaded/" + fileName);
+      let imagePath = `images/uploaded/${fileName}`;
+      await db.query("UPDATE users SET image = $1 WHERE name = $2", [
+        imagePath,
+        user.username,
+      ]);
+    }
+    res.redirect("signin");
+  }
+};
